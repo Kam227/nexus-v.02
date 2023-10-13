440af1fef31b61cfb87eadedd5aebc5777f04c72
Kam Williams <107746396+Kam227@users.noreply.github.com>
2023-10-12 20:42:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBECXYTGQykyurvFdRRSZPtN69vVmbvlAU",
-    authDomain: "interlinked-5b77a.firebaseapp.com",
-    projectId: "interlinked-5b77a",
-    storageBucket: "interlinked-5b77a.appspot.com",
-    messagingSenderId: "926924551340",
-    appId: "1:926924551340:web:d34613daef774b8a0b1b09",
-    measurementId: "G-GC00B6XQSD"
+        // API Firebase Information 
   }
 
   if (!firebase.apps.length){
